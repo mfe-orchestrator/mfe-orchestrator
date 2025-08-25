3c3611f33ec9a72dfb979b6fa5191d79698078cb
Ilaria <ilaria.ciavarella@hotmail.com>
2025-08-25 19:36:49 +0200

    wip: api key auth

--- backend/src/plugins/autorization.ts
+++ backend/src/plugins/autorization.ts
@@ -1,20 +1,21 @@
-import { FastifyInstance } from 'fastify';
-import fastifyPlugin from 'fastify-plugin';
-import AuthenticationError from '../errors/AuthenticationError';
-import UserModel, { getSecret, ISSUER } from '../models/UserModel';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import { redisClient } from './redis';
-import axios from 'axios';
-import UserService from '../service/UserService';
+import { FastifyInstance, FastifyRequest } from "fastify"
+import fastifyPlugin from "fastify-plugin"
+import AuthenticationError from "../errors/AuthenticationError"
+import UserModel, { getSecret, ISSUER } from "../models/UserModel"
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { redisClient } from "./redis"
+import axios from "axios"
+import UserService from "../service/UserService"
+import ApiKey from "../models/ApiKeyModel"
 
-interface AuthUserDTO{
-  name?: string
-  surname?: string
-  email: string
-  id?: string
+interface AuthUserDTO {
+    name?: string
+    surname?: string
+    email: string
+    id?: string
 }
 
-const TOKEN_EXPIRATION = 60 * 60;
+const TOKEN_EXPIRATION = 60 * 60
 // export const userHasGrants = (requiredGrants?: string[], userGrants?: string[]) => {
 //   if (!requiredGrants) return true;
 //   if (!userGrants) return false;
@@ -49,146 +50,166 @@ const TOKEN_EXPIRATION = 60 * 60;
 // };
 
 const getDataFromGoogle = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO> => {
-  if(redisClient){
-    const userFromCache = await redisClient.get(authToken)
-    if(userFromCache){
-      return JSON.parse(userFromCache);
+    if (redisClient) {
+        const userFromCache = await redisClient.get(authToken)
+        if (userFromCache) {
+            return JSON.parse(userFromCache)
+        }
     }
-  }
-  
-  const response = await axios.get(`https://www.googleapis.com/oauth2/v1/tokeninfo`, {
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },  
-  });
-  if(redisClient){
-    await redisClient.set(authToken, JSON.stringify(response.data), { EX: TOKEN_EXPIRATION });
-  }
-  return response.data;
-}
 
-const getDataFromAuth0 = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO>  => {
-  const decodedToken = jwt.decode(authToken, { json: true,  complete: true });
-  if(!decodedToken){
-    throw new AuthenticationError('Invalid token');
-  }
-  const payload = decodedToken.payload as JwtPayload
-  const sub = payload.sub
-  if(redisClient && sub){
-    const userFromCache = await redisClient.get(sub)
-    if(userFromCache){
-      return JSON.parse(userFromCache);
+    const response = await axios.get(`https://www.googleapis.com/oauth2/v1/tokeninfo`, {
+        headers: {
+            Authorization: `Bearer ${authToken}`
+        }
+    })
+    if (redisClient) {
+        await redisClient.set(authToken, JSON.stringify(response.data), { EX: TOKEN_EXPIRATION })
     }
-  }
-
-  const response = await axios.get(`https://${fastify.config.AUTH0_DOMAIN}/userinfo`, {
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-  });
-
-  const realResponse = {
-    ...response.data,
-    name: response.data.family_name,
-    surname: response.data.given_name
-  }
-
-  if(redisClient && sub){
-    await redisClient.set(sub, JSON.stringify(realResponse), { EX: TOKEN_EXPIRATION });
-  }
-  return realResponse
+    return response.data
 }
 
-const getDataFromLocal = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO>  => {
-  const decodedToken = jwt.verify(authToken, getSecret(), { complete: true });
-  if(!decodedToken){
-    throw new AuthenticationError('Invalid token');
-  }
-  const payload = decodedToken.payload as JwtPayload
-  return {email: payload.email, id: payload.id};
+const getDataFromAuth0 = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO> => {
+    const decodedToken = jwt.decode(authToken, { json: true, complete: true })
+    if (!decodedToken) {
+        throw new AuthenticationError("Invalid token")
+    }
+    const payload = decodedToken.payload as JwtPayload
+    const sub = payload.sub
+    if (redisClient && sub) {
+        const userFromCache = await redisClient.get(sub)
+        if (userFromCache) {
+            return JSON.parse(userFromCache)
+        }
+    }
+
+    const response = await axios.get(`https://${fastify.config.AUTH0_DOMAIN}/userinfo`, {
+        headers: {
+            Authorization: `Bearer ${authToken}`
+        }
+    })
+
+    const realResponse = {
+        ...response.data,
+        name: response.data.family_name,
+        surname: response.data.given_name
+    }
+
+    if (redisClient && sub) {
+        await redisClient.set(sub, JSON.stringify(realResponse), { EX: TOKEN_EXPIRATION })
+    }
+    return realResponse
 }
 
-const getDataFromMsal = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO>  => {
-  const decodedToken = jwt.decode(authToken, { json: true,  complete: true });
-  if(!decodedToken){
-    throw new AuthenticationError('Invalid token');
-  }
-  const payload = decodedToken.payload as JwtPayload
-  return {
-    email: payload.email || payload.preferred_username, 
-    name: payload.name
-  };
+const getDataFromLocal = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO> => {
+    const decodedToken = jwt.verify(authToken, getSecret(), { complete: true })
+    if (!decodedToken) {
+        throw new AuthenticationError("Invalid token")
+    }
+    const payload = decodedToken.payload as JwtPayload
+    return { email: payload.email, id: payload.id }
+}
+
+const getDataFromMsal = async (fastify: FastifyInstance, authToken: string): Promise<AuthUserDTO> => {
+    const decodedToken = jwt.decode(authToken, { json: true, complete: true })
+    if (!decodedToken) {
+        throw new AuthenticationError("Invalid token")
+    }
+    const payload = decodedToken.payload as JwtPayload
+    return {
+        email: payload.email || payload.preferred_username,
+        name: payload.name
+    }
 }
 
 const getUserDataFromToken = async (fastify: FastifyInstance, authToken: string, issuer: string) => {
-  switch(issuer){
-    case "google":
-      return getDataFromGoogle(fastify, authToken);
-    case "auth0":
-      return getDataFromAuth0(fastify, authToken);
-    default:
-      const decodedToken = jwt.decode(authToken, { json: true,  complete: true });
-      if(!decodedToken){
-        throw new AuthenticationError('Invalid token');
-      }
-      const payload = decodedToken.payload as JwtPayload
-      if(!payload.exp || payload.exp < Date.now() / 1000){
-        throw new AuthenticationError('Token expired');
-      }
-      if(payload.iss == ISSUER){
-        return getDataFromLocal(fastify, authToken);
-      }else if(payload.iss == `https://login.microsoftonline.com/${fastify.config.AZURE_ENTRAID_TENANT_ID}/v2.0`){
-        return getDataFromMsal(fastify, authToken);  
-      }
-      
-  }
+    switch (issuer) {
+        case "google":
+            return getDataFromGoogle(fastify, authToken)
+        case "auth0":
+            return getDataFromAuth0(fastify, authToken)
+        default:
+            const decodedToken = jwt.decode(authToken, { json: true, complete: true })
+            if (!decodedToken) {
+                throw new AuthenticationError("Invalid token")
+            }
+            const payload = decodedToken.payload as JwtPayload
+            if (!payload.exp || payload.exp < Date.now() / 1000) {
+                throw new AuthenticationError("Token expired")
+            }
+            if (payload.iss == ISSUER) {
+                return getDataFromLocal(fastify, authToken)
+            } else if (payload.iss == `https://login.microsoftonline.com/${fastify.config.AZURE_ENTRAID_TENANT_ID}/v2.0`) {
+                return getDataFromMsal(fastify, authToken)
+            }
+    }
+}
+
+const checkApiKey = async (fastify: FastifyInstance, request: FastifyRequest) => {
+    const apiKey = request.headers["api-key"] || (request.query as any)["apiKey"]
+    if (!apiKey) {
+        throw new AuthenticationError("API key not found")
+    }
+
+    const apiKeyFromDb = (await ApiKey.find()).find(async candidateApiKey => {
+        return await candidateApiKey.compareApiKey(apiKey)
+    })
+
+    if (!apiKeyFromDb) {
+        throw new AuthenticationError("API key not found")
+    }
 }
 
 export default fastifyPlugin(
-  async (fastify: FastifyInstance) => {
-    fastify.addHook('preHandler', async (request, response) => {
-      if (request.routeOptions.config.public || request.routeOptions.url?.startsWith('/api-docs')) {
-        return;
-      }
-      fastify.log.debug('Pre handler login ok');
-      const authToken = request?.headers?.['authorization']?.replace('Bearer ', '');
-      fastify.log.debug('Auth token', authToken);
-      if (!authToken) {
-        throw new AuthenticationError('Missing or invalid Authorization header');
-      }
-      const issuer = request.headers['issuer'] as string || ISSUER
-      const userData = await  getUserDataFromToken(fastify, authToken, issuer)
-      if(!userData){
-        throw new AuthenticationError('User not found form JWT');
-      }
+    async (fastify: FastifyInstance) => {
+        fastify.addHook("preHandler", async (request, response) => {
+            if (request.routeOptions.config.public || request.routeOptions.url?.startsWith("/api-docs")) {
+                return
+            }
 
-      let user = await UserModel.findOne({ email: userData.email });
-      const isFederatedAuth = issuer != ISSUER
-      if(user?.activateEmailToken){
-        if(user?.activateEmailExpires && user.activateEmailExpires < new Date()){
-          throw new AuthenticationError('User not verified and the invitation is expired, please reset your password');
-        }
-        throw new AuthenticationError('User not verified, please verify your email');
-      }
-      if (!user) {
-        if(isFederatedAuth){
-          //Now i will auto provision the user in the system 
-          user = await new UserService().register({
-            email: userData.email,
-            name: userData.name,
-            surname: userData.surname
-          }, false)
-        }else{
-          throw new AuthenticationError(
-            'User found in authentication provider but not in database with email ' + userData.email
-          );
-        }
-      }
-      
-      request.databaseUser = {
-        ...user.toObject(),
-      };      
-    });
-  },
-  { name: 'authorization', dependencies: ['config'] }
-);
+            if (1 === 1) {
+                checkApiKey(fastify, request)
+            }
+
+            fastify.log.debug("Pre handler login ok")
+            const authToken = request?.headers?.["authorization"]?.replace("Bearer ", "")
+            fastify.log.debug("Auth token", authToken)
+            if (!authToken) {
+                throw new AuthenticationError("Missing or invalid Authorization header")
+            }
+            const issuer = (request.headers["issuer"] as string) || ISSUER
+            const userData = await getUserDataFromToken(fastify, authToken, issuer)
+            if (!userData) {
+                throw new AuthenticationError("User not found form JWT")
+            }
+
+            let user = await UserModel.findOne({ email: userData.email })
+            const isFederatedAuth = issuer != ISSUER
+            if (user?.activateEmailToken) {
+                if (user?.activateEmailExpires && user.activateEmailExpires < new Date()) {
+                    throw new AuthenticationError("User not verified and the invitation is expired, please reset your password")
+                }
+                throw new AuthenticationError("User not verified, please verify your email")
+            }
+            if (!user) {
+                if (isFederatedAuth) {
+                    //Now i will auto provision the user in the system
+                    user = await new UserService().register(
+                        {
+                            email: userData.email,
+                            name: userData.name,
+                            surname: userData.surname
+                        },
+                        false
+                    )
+                } else {
+                    throw new AuthenticationError("User found in authentication provider but not in database with email " + userData.email)
+                }
+            }
+
+            request.databaseUser = {
+                ...user.toObject()
+            }
+        })
+    },
+    { name: "authorization", dependencies: ["config"] }
+)
