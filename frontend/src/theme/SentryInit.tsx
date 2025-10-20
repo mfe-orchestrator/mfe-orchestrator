8b03ff615c32c1d0d51c1c62b8acb53432c4997f
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2025-10-20 19:40:17 +0200

    Sentry fix

--- frontend/src/theme/SentryInit.tsx
+++ frontend/src/theme/SentryInit.tsx
@@ -10,13 +10,17 @@ const SentryInit : React.FC<React.PropsWithChildren>= ({children}) =>{
         return
     }
 
+    if(Sentry.isInitialized()) return;
+
       Sentry.init({
           dsn: window.globalConfiguration.SENTRY_DSN,
           // Setting this option to true will send default PII data to Sentry.
           // For example, automatic IP address collection on events
           enableLogs: true,
           enabled: true,
-          sendDefaultPii: true
+          sendDefaultPii: true,
+          release: window.globalConfiguration.VERSION,
+          environment: window.globalConfiguration.ENVIRONMENT
       });
       console.log("Sentry initialized");
   }, [window.globalConfiguration.SENTRY_DSN])
