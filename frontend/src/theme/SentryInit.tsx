f0ea1cc49c963a00911ff94d50091082d07a323d
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2025-10-21 10:43:16 +0200

    Added replay integration

--- frontend/src/theme/SentryInit.tsx
+++ frontend/src/theme/SentryInit.tsx
@@ -24,10 +24,14 @@ const SentryInit : React.FC<React.PropsWithChildren>= ({children}) =>{
           integrations: [
             Sentry.feedbackIntegration({
               colorScheme: "system",
-
             }),
-          ]
+            Sentry.replayIntegration()
+          ],
+          // Session Replay
+          replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+          replaysOnErrorSampleRate: 0.5 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
       });
+      
       console.log("Sentry initialized");
   }, [window?.globalConfiguration?.SENTRY_DSN])
 
