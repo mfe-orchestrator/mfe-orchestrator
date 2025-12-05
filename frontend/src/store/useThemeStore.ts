61bd6db61fd7d281c4d38e4a40eabcce512abf11
Lorenzo De Francesco <lorenzo.defrancesco@step4business.com>
2025-12-06 00:37:32 +0100

    feat: theme system and language eng

--- frontend/src/store/useThemeStore.ts
+++ frontend/src/store/useThemeStore.ts
@@ -1,49 +1,51 @@
-import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
-import React from 'react';
-import { Locale } from 'date-fns';
-import { it, fr, de, es, enUS } from 'date-fns/locale';
+import { Locale } from "date-fns"
+import { de, enUS, es, fr, it } from "date-fns/locale"
+import React from "react"
+import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 
 export enum ThemeEnum {
-    LIGHT = 'LIGHT',
-    DARK = 'DARK',
-    SYSTEM = 'SYSTEM'
+    LIGHT = "LIGHT",
+    DARK = "DARK",
+    SYSTEM = "SYSTEM"
 }
 
 interface ThemeState {
-    icon?: React.ReactNode;
-    title?: String | React.ReactNode;
-    setTitle: (title: String | React.ReactNode, icon?: React.ReactNode) => void;
+    icon?: React.ReactNode
+    title?: String | React.ReactNode
+    setTitle: (title: String | React.ReactNode, icon?: React.ReactNode) => void
 
-    theme: ThemeEnum;
-    setTheme: (theme: ThemeEnum) => void;
+    theme: ThemeEnum
+    setTheme: (theme: ThemeEnum) => void
 
-    language: string;
-    setLanguage: (language: string) => void;
+    language: string
+    setLanguage: (language: string) => void
 
-    getLocale: () => Locale;
+    getLocale: () => Locale
 }
 
 const useThemeStore = create<ThemeState>()(
-  devtools(
-    (set, get) => ({
-      setTitle: (title: String | React.ReactNode, icon?: React.ReactNode) => {
-        set({ title, icon });
-      },
-      setTheme: (theme: ThemeEnum) => {
-        set({ theme });
-      },
-      setLanguage: (language: string) => {
-        set({ language });
-      },
-      getLocale: () => {
-        return get().language === 'it' ? it : get().language === 'fr' ? fr : get().language === 'de' ? de : get().language === 'es' ? es : enUS;
-      }
-    }),
-    {
-      name: 'theme-storage',
-    }
-  )
-);
+    devtools(
+        (set, get) => ({
+            theme: ThemeEnum.SYSTEM,
+            language: "en",
+            setTitle: (title: String | React.ReactNode, icon?: React.ReactNode) => {
+                set({ title, icon })
+            },
+            setTheme: (theme: ThemeEnum) => {
+                set({ theme })
+            },
+            setLanguage: (language: string) => {
+                set({ language })
+            },
+            getLocale: () => {
+                return get().language === "it" ? it : get().language === "fr" ? fr : get().language === "de" ? de : get().language === "es" ? es : enUS
+            }
+        }),
+        {
+            name: "theme-storage"
+        }
+    )
+)
 
 export default useThemeStore
